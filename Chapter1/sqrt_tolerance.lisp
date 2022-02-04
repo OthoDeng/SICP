f7a317d43897a848998d8977f19ae85f86043b21
ottoqwq <ottodeng9@gmail.com>
2022-02-04 18:16:22 +0800

    added sqrt_tolerance file

---
+++ Chapter1/sqrt_tolerance.lisp
@@ -0,0 +1,10 @@
+;using fixed-point method to calculate the square root
+(define (fixed-point f start)
+    (define tolerance 0 0.00001))
+    (define (close-enuf? u v)
+        (< (abs (- u v)) tolerance))
+    (define (iter old new)
+        (if (close-enuf? old new)
+            new
+            (iter new (f new))))
+    (iter start (f start)))
