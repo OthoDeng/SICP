e205d4d05ccdbdddbbc302e2f2fc885930d33fe9
ottoqwq <ottodeng9@gmail.com>
2022-03-04 23:21:38 +0800

    added "sine" file

---
+++ Chapter1/computing_sine.lisp
@@ -0,0 +1,16 @@
+#lang sicp
+;The sine of an angle can be computed by making use of the approximation:
+;   sinx = x if x is sufficiently small, which can be resolved in；
+;   sinx = 3sin(x/3) - 4sin^2(x/3)
+;And that "sufficient small" here we define it when its magnitude is not greater than 0.1 radians
+
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+    (if (not (> (abs angle) 0.1))
+        angle
+        (p (sine (/ angle 3.0)))))
+
+;given a angle( such as <sine 12.15>) try calculate how many times that the procedure p was applied
