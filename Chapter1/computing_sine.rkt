d9483b40fc1f7a636693e34d31431b1915ee6d52
ottoqwq <ottodeng9@gmail.com>
2022-03-06 10:49:28 +0800

    modified standard

--- Chapter1/computing_sine.rkt
+++ Chapter1/computing_sine.rkt
@@ -9,8 +9,8 @@
 (define (p x) (- (* 3 x) (* 4 (cube x))))
 
 (define (sine angle)
-    (if (not (> (abs angle) 0.1))
-        angle
-        (p (sine (/ angle 3.0)))))
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
 
-;given a angle( such as <sine 12.15>) try calculate how many times that the procedure p was applied
+;question: given a angle( such as <sine 12.15>) and try calculate how many times that the procedure p was applied
