cfd9b4ae6faf4438905c76b36de149ff2a31c29f
ottoqwq <ottodeng9@gmail.com>
2022-06-03 14:56:38 +0800

    modirfied：     improved_fib.rkt

--- Chapter1/improved_fib.rkt
+++ Chapter1/improved_fib.rkt
@@ -22,7 +22,7 @@
 				   (+ (square p) (square q))
 				   (+ (* 2 p q) (square q))
 				   (/ count 2)))
-		(eles (fib-iter (+ (* b q) (* a q) (* a p))
+		(else (fib-iter (+ (* b q) (* a q) (* a p))
 						(+ (* b p) (* a q))
 						p
 						q
